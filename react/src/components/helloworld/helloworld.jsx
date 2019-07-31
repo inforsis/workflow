8f4be8723b153fbdd350075fce049464f6cf9ded
inforsis <inforsis@gmail.com>
2019-07-30 22:09:02 -0300

    atualizada a estrutura das pastas de projetos

---
+++ react/src/components/helloworld/helloworld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './scss/main.scss';
+
+import './js/main.js';
+
+import logoReact from './img/logo-react.png'; 
+
+const helloworld = () => <span>Webpack, Babel 7+, react!!!<i className="material-icons">favorite</i></span>
+
+export default helloworld;
---
+++ react/src/scss/tools/README.txt
@@ -0,0 +1,10 @@
+TOOLS
+	
+
+	Camada com mixins, funcoes, placeholders, etc:
+		
+		mixins de font-face, até mixins de animações, placeholders, etc;
+
+		importacao de fonts;
+
+		nada é gerado diretamente como CSS final também.
